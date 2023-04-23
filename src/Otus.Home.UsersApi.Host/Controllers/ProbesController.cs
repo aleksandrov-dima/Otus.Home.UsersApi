e2423d796fe0045cf3518cdca26ddd97bea80aee
Dmitriy Aleksandrov <d.aleksandrov@pravo.tech>
2023-04-23 13:47:43 +0300

    Добавление Dockerfile, deployment_api.yml, service.yml, ProbesController.cs

---
+++ src/Otus.Home.UsersApi.Host/Controllers/ProbesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Otus.Home.UsersApi.Core.Domain.Administration;
+using Otus.Home.UsersApi.Host.Models;
+
+namespace Otus.Home.UsersApi.Host.Controllers
+{
+	/// <summary>
+	/// Пользователи
+	/// </summary>
+	[ApiController]
+	public class ProbesController : ControllerBase
+	{
+		/// <summary>
+		/// Точка для хелсчека
+		/// </summary>
+		[HttpGet]
+		[Route("health")]
+		public Task<ActionResult> GetHealth()
+		{
+			return Task.FromResult<ActionResult>(Ok(new
+			{
+				status = "OK"
+			}));
+		}
+	}
+}
